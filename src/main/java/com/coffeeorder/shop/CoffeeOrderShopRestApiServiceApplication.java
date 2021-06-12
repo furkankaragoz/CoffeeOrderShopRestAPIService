d19f347a26bb8048db3f7d3964478799ec314e6a
Furkan KARAGÖZ <furkannkaragozz@gmail.com>
2021-06-12 15:33:30 +0300

    Initial commit for CoffeeOrderShopRestApiServiceApplication conf.

---
+++ src/main/java/com/coffeeorder/shop/CoffeeOrderShopRestApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coffeeorder.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOrderShopRestApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeOrderShopRestApiServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coffeeorder/shop/CoffeeOrderShopRestApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeeorder.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeOrderShopRestApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
